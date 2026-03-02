22aa636d501d92120329124d8530b048731ae7f5
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-02 20:22:47 +0530

    initialize notes and i will continuously work after this commit everyday like a good developer

--- scalableAndProductionAdvanceBackend/src/app.js
+++ scalableAndProductionAdvanceBackend/src/app.js
@@ -6,4 +6,15 @@ app.get("/", (req, res)=>{
     res.send("running on api endpoint");
 });
 
+const notes = [{
+    title: test-title,
+    description: test-description
+}];
+
+app.post("/notes", (req, res) => {
+    console.log(req.body);
+    res.send(notes);
+})
+
+
 export {app};
