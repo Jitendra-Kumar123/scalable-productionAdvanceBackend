cfc485bb7c054b32a92de2e7cd9ae9b6638851b4
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-05 21:11:10 +0530

    made the api endpoints of imp methods and also learn dynamic routing

--- scalableAndProductionAdvanceBackend/src/app.js
+++ scalableAndProductionAdvanceBackend/src/app.js
@@ -1,19 +1,52 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res)=>{
     res.send("running on api endpoint");
 });
 
-const notes = [{
-    title: test-title,
-    description: test-description
-}];
+const notes = [];
 
 app.post("/notes", (req, res) => {
-    console.log(req.body);
-    res.send(notes);
+    notes.push(req.body);
+
+    res.status(201).json({
+        message: "note created successfully"
+    })
+})
+
+app.get("/notes", (req, res)=>{
+    res.status(200).json({
+        message: "notes fetched successfully",
+        notes: notes
+    })
+})
+
+app.delete("/notes/:index", (req, res) => {
+    const index = req.params.index
+    console.log(req.params)
+    delete notes [index]
+
+    res.status(200).json({
+        message: "note deleted successfully"
+    })
+})
+
+app.patch("/notes/:index", (req, res) => {
+    const index = req.params.index
+
+    const {title, description} = req.body
+
+    notes[index].title = title
+    notes[index].description = description
+
+    console.log(notes[index])
+
+    res.status(200).json({
+        message: "note updated successfully"
+    })
 })
 
 
