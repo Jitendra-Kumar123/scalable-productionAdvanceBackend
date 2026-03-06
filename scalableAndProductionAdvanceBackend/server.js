92f684263e8e930c86ca714a41938f8dd534b0d9
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-06 17:59:04 +0530

    notes data modelling

--- scalableAndProductionAdvanceBackend/server.js
+++ scalableAndProductionAdvanceBackend/server.js
@@ -1,4 +1,7 @@
 import {app} from "./src/app.js"
+import { ConnectToDB } from "./src/DB/DB.js";
+
+ConnectToDB();
 
 app.listen(3000, ()=>{
     console.log("running||professional server is running")
---
+++ scalableAndProductionAdvanceBackend/src/Models/Note.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+export const noteModel = mongoose.model("Note", noteSchema);
