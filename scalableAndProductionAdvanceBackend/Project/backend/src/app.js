405f95a86487bd4b853ab329004903056786eb73
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-28 19:41:45 +0530

    all source code

--- scalableAndProductionAdvanceBackend/Project/backend/src/app.js
+++ scalableAndProductionAdvanceBackend/Project/backend/src/app.js
@@ -1,17 +1,19 @@
 import express from "express"
 const app = express();
 import "dotenv/config"
+import cors from 'cors'
 import multer from "multer"
 import { uploadFile } from "./services/storage.service.js"
 import postModel from "./model/post.model.js"
 
+app.use(cors());
 app.use(express.json());
 
 const upload = multer({storage: multer.memoryStorage()})
 
 app.post("/create-post", upload.single("image"), async(req, res)=>{
 
-    const result = await uploadFile(req.file);
+    const result = await uploadFile(req.file.buffer);
     
     const post = await postModel.create({
         image: result.url,
