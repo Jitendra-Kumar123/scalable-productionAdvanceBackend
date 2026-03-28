b3870511f860d5d81f498defe10643f801fd94c8
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-28 19:43:40 +0530

    all frontend ui source code

--- scalableAndProductionAdvanceBackend/Project/frontend/src/App.jsx
+++ scalableAndProductionAdvanceBackend/Project/frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import CreatePost from './pages/CreatePost'
+import FeedPage from './pages/FeedPage'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/create-post' element={<CreatePost/>}></Route>
+        <Route path='/posts' element={<FeedPage/>}></Route>
+        <Route path='/' element={<h1>default page temp</h1>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
