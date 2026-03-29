a9e451816330e5afa2fdc33e9134513383041816
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-29 18:30:39 +0530

    build a logic for registering a user with generating a token for server

--- scalableAndProductionAdvanceBackend/AuthSystem/src/controllers/Auth.controller.js
+++ scalableAndProductionAdvanceBackend/AuthSystem/src/controllers/Auth.controller.js
@@ -1,9 +1,25 @@
 import userModel from "../model/user.model.js"
+import jwt from "jsonwebtoken"
 
 
 async function registerUser(req, res){
     const {userName, email, password} = req.body;
 
+    const user = await userModel.create({
+        userName, email, password
+    })
+
+    const token = jwt.sign({
+        id: user._id
+    },
+        process.env.JWT_SECRET
+    )
+
+    res.status(201).json({
+        message: "user registered successfully with token",
+        user, 
+        token
+    })
 }
 
 export default {
