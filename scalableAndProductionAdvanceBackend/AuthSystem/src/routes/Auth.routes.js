96e390dfcc405edcd421524081ae147d7a4b9c07
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-29 17:28:03 +0530

    fixing routes export

--- scalableAndProductionAdvanceBackend/AuthSystem/src/routes/Auth.routes.js
+++ scalableAndProductionAdvanceBackend/AuthSystem/src/routes/Auth.routes.js
@@ -2,6 +2,6 @@ import express from "express"
 const router = express.Router();
 import authController from "../controllers/Auth.controller.js"
 
-router.post("/registerUser", authController.registerUser);
+router.post("/register-user", authController.registerUser);
 
-export default {router};
+export default router;
