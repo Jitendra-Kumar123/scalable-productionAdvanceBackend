ca8c7a8bb306eec8511d3d94b9c914f09294720a
Jitendra Kumar <jitendra0202006@gmail.com>
2026-03-30 16:08:03 +0530

    checking if user already exists

--- scalableAndProductionAdvanceBackend/AuthSystem/src/controllers/Auth.controller.js
+++ scalableAndProductionAdvanceBackend/AuthSystem/src/controllers/Auth.controller.js
@@ -5,6 +5,17 @@ import jwt from "jsonwebtoken"
 async function registerUser(req, res){
     const {userName, email, password} = req.body;
 
+    const isUserAlreadyExists = await userModel.findOne({
+        email: email
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+        message: "user already exists"
+    })
+    }
+
+
     const user = await userModel.create({
         userName, email, password
     })
@@ -15,10 +26,11 @@ async function registerUser(req, res){
         process.env.JWT_SECRET
     )
 
+    res.cookie("token", token);
+
     res.status(201).json({
         message: "user registered successfully with token",
-        user, 
-        token
+        user
     })
 }
 
