8936774938c07203dcb8b6f86d4ce21416b8b1bf
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-02 00:00:00 +0530

    all server instances

--- scalableAndProductionAdvanceBackend/spotifybackendcomplete/src/app.js
+++ scalableAndProductionAdvanceBackend/spotifybackendcomplete/src/app.js
@@ -1,7 +1,9 @@
 import express from "express"
 const app = express();
+import authRoutes from "./routes/auth.routes.js"
 
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 export {app};
