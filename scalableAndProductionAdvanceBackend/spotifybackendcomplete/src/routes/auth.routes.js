0448868c8a40b9a5c6039272183499f4c6e39351
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-02 22:39:32 +0530

    auth route

--- scalableAndProductionAdvanceBackend/spotifybackendcomplete/src/routes/auth.routes.js
+++ scalableAndProductionAdvanceBackend/spotifybackendcomplete/src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from "express"
-import authController from "../controllers/auth.controller";
+import authController from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register-user", authController.registerUser);
+router.post("/login-user", authController.loginUser);
+
 
 export default router;
