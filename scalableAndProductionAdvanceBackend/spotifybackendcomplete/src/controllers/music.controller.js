c2fe06e2208a8d458f7ad1ba70fe57ef667e260d
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-03 23:14:15 +0530

    music controller

--- scalableAndProductionAdvanceBackend/spotifybackendcomplete/src/controllers/music.controller.js
+++ scalableAndProductionAdvanceBackend/spotifybackendcomplete/src/controllers/music.controller.js
@@ -1,4 +1,6 @@
 import musicModel from "../model/music.model.js"
+import jwt from "jsonwebtoken"
+import uploadFile from "../services/storage.service.js";
 
 async function createMusic(req, res){
     const token = req.cookies.token;
@@ -9,7 +11,41 @@ async function createMusic(req, res){
         })
     }
 
-    
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(decoded.role !== "artist"){
+            return res.status(403).json({
+                message: "You don't have access to create an music"
+            })
+        }
+    }
+    catch(err){
+        return res.status(401).json({
+            message: "unauthorized"
+        })
+    }
+
+    const {title} = req.body;
+    const file = req.file;
+
+    const result = await uploadFile(file.buffer.toString('base64'));
+
+    const music = await musicModel.create({
+        uri: result.url,
+        title,
+        artist: decoded.id
+    })
+
+    res.status(201).json({
+        message: "music uploaded successfully",
+        music: {
+            id: music._id,
+            uri: music.uri,
+            title: music.title,
+            artist: music.artist
+        }
+    })
 }
 
 export default {createMusic};
