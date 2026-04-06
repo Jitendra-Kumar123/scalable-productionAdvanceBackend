bf4da2dbd2107f768d683f9d6a31bab354fe67ee
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-06 14:46:56 +0530

    jest file for test

--- scalableAndProductionAdvanceBackend/TestingPackage/src/test/__app.test.js
+++ scalableAndProductionAdvanceBackend/TestingPackage/src/test/__app.test.js
@@ -2,10 +2,25 @@ const request = require("supertest")
 const app = require("../app")
 
 
-describe("GET /test", ()=>{
-    it("should be 200 ok", async()=>{
-        const res = await request(app).get("/test");
+// describe("GET /test", ()=>{
+//     it("should be 200 ok", async()=>{
+//         const res = await request(app).get("/test");
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.message).toEqual("this is test api for testing the jest");
+//     })
+// })
+
+describe("POST /register", () => {
+    it("should be 200 ok", async () => {
+        const res = await request(app).post("/register").send({
+            
+                userName: "Test_User",
+                email: "test@example.com",
+                password: "Password@123"
+            
+    })
+
         expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual("this is test api for testing the jest");
+        expect(res.body.message).toEqual("User Registered Successfully");
     })
 })
