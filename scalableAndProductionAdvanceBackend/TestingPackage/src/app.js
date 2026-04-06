df910d63fb4a5693a8c6fef63e328dfc8f8c9bb3
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-06 14:48:00 +0530

    imports

--- scalableAndProductionAdvanceBackend/TestingPackage/src/app.js
+++ scalableAndProductionAdvanceBackend/TestingPackage/src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const validationRules = require("./middlewares/validation.middleware.js");
 
-app.get("/test", (req, res)=>{
+app.use(express.json());
+
+// app.get("/test", (req, res)=>{
+//     res.status(200).json({
+//         message: "this is test api for testing the jest"
+//     });
+// })
+
+app.post("/register",validationRules.registerUserValidationRules, (req, res) => {
+    const {userName, email, password} = req.body;
+
     res.status(200).json({
-        message: "this is test api for testing the jest"
+        message: "User Registered Successfully",
+        user: {
+            userName, email
+        }
     })
 })
 
