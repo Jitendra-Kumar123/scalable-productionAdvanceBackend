66a1df48093ba6dc93697e68ca39856091a3591b
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-08 00:23:29 +0530

    updated file with Advance technique

--- AdvanceAuthComplete/config/config.js
+++
@@ -1,6 +0,0 @@
-import { config } from "dotenv"
-import "dotenv/config"
-
-const config = {
-    
-}
--- AdvanceAuthComplete/config/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-async function ConnectDB(){
-    await mongoose.connect(process.env.MONGO_URI);
-}
-
-export default ConnectDB;
