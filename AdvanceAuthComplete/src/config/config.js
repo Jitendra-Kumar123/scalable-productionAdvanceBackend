1a000fa3d0193395a96f49036cdaaa29b9426ade
Jitendra Kumar <jitendra0202006@gmail.com>
2026-04-13 14:04:32 +0530

    this is for new developer

--- AdvanceAuthComplete/src/config/config.js
+++ AdvanceAuthComplete/src/config/config.js
@@ -1,11 +1,16 @@
 import "dotenv/config"
 
-if(!process.env.MONGO_URI){
-    throw new Error("MONGO_URI is not defined in environment variable");
+if(!process.env.MONGODB_URI){
+    throw new Error("MONGODB_URI is not defined in environment variable");
+}
+
+if(!process.env.JWT_SECRET){
+    throw new Error("JWT_SECRET is not defined in environment variable");
 }
 
 const config = {
-    MONGO_URI: process.env.MONGO_URI
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
 }
 
 export {config};
