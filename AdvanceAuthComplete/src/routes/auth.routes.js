882679a7bf2eb7d61ebdec6889dfa3022f9a71e4
Jitendra Kumar <jitendra0202006@gmail.com>
2026-06-06 14:44:45 +0530

    build logout route features

--- AdvanceAuthComplete/src/routes/auth.routes.js
+++ AdvanceAuthComplete/src/routes/auth.routes.js
@@ -5,5 +5,6 @@ import * as authController from "../controllers/auth.controller.js"
 authRouter.post("/register", authController.register);
 authRouter.get("/get-me", authController.getMe);
 authRouter.get("/refresh-token", authController.refreshToken);
+authRouter.get("/logout", authController.logout);
 
 export default authRouter;
