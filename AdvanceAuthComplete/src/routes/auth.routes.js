8f801b0e963ac4a336bb53bee6455254c15dbf71
Jitendra Kumar <jitendra0202006@gmail.com>
2026-06-06 15:46:43 +0530

    route login

--- AdvanceAuthComplete/src/routes/auth.routes.js
+++ AdvanceAuthComplete/src/routes/auth.routes.js
@@ -3,8 +3,15 @@ const authRouter = express.Router();
 import * as authController from "../controllers/auth.controller.js"
 
 authRouter.post("/register", authController.register);
+
 authRouter.get("/get-me", authController.getMe);
+
 authRouter.get("/refresh-token", authController.refreshToken);
+
 authRouter.get("/logout", authController.logout);
+
+authRouter.get("/logoutAllDevices", authController.logoutAllDevices);
+
+authRouter.post("/login", authController.login);
 
 export default authRouter;
